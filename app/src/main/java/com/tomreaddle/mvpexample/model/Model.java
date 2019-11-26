34d92bea343f67fc6ecf524dcf96bfe96b2aa8ac
Usmanov8102 <elusm17@gmail.com>
2019-11-26 23:30:32 +0500

    that's all

--- app/src/main/java/com/tomreaddle/mvpexample/model/Model.java
+++ app/src/main/java/com/tomreaddle/mvpexample/model/Model.java
@@ -35,6 +35,7 @@ public class Model implements Presenter {
         Log.d(TAG, "performLogin: "+APIusername + "\n" + APIpassword);
     }
 
+
     public boolean isValidEmailAddress(String email) {
         String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
